0f1d7b3506d6a8a1c8d032bdd83ca521adc3f856
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-27 19:41:48 -0300

    [feature] perform segue

---
+++ screenNavigation/ViewControllerYellow.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerYellow.swift
+//  screenNavigation
+//
+//  Created by Agustin Carbajal on 27/10/2023.
+//
+
+import UIKit
+
+class ViewControllerYellow: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
